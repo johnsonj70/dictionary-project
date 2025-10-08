693604d97a1c84ee185ce3897ea37b0f26ac42e1
johnsonj70 <johnson.janice30@yahoo.co.uk>
2025-10-08 14:22:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from "./image/logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
