5470dcedc9e368a6bcfe99a71afca5c44413d665
johnsonj70 <johnson.janice30@yahoo.co.uk>
2025-10-08 16:54:05 +0100

    Added form to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 import logo from "./image/logo.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+            <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <p>
+            This is a React.js Project coded by <a href="https://github.com/johnsonj70" target="blank" rel="noreferrer">Janice Johnson,</a>{" "}
+            is open-sourced on{" "}
+            <a href="https://github.com/johnsonj70/dictionary-project" target="blank" rel="noreferrer">GitHub</a>{" "} and hosted on{" "}
+            <a href="https://jj-dictionary-project.netlify.app/" target="_blank" rel="noreferrer">Netlify</a> 
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyWord, setKeyWord] = useState("");
+
+    function search (event) {
+        event.preventDefault();
+        alert(`Searching for the definition of ${keyWord}`);
+    }
+
+    function handleKeyWordChange(event) {
+        setKeyWord(event.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeyWordChange}/>
+
+            </form>
+        </div>
+    );
+}
