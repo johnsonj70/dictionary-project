274ecd6669ca3b25202f1c8819b07156a50eb000
johnsonj70 <johnson.janice30@yahoo.co.uk>
2025-10-11 10:00:11 +0100

    Adjusted formatting on headings

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,9 +4,10 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
+        
          <ul className="Synonyms">
             {props.synonyms.map(function (synonym, index) {
-                return <li key={index}>{synonym}</li>;
+                return <span><li key={index}>{synonym}</li></span>;
             })}
         </ul>
     );
