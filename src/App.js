1f29ebea31cb36ffd9158b5c105aa166bb2872e1
johnsonj70 <johnson.janice30@yahoo.co.uk>
2025-10-15 21:40:26 +0100

    Removed logo from homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import logo from "./image/logo.png"
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
         <main>
             <div className="container">
               <Dictionary defaultKeyWord = "sunset" />
